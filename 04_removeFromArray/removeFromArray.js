b585d3b480fc041fe0f5a44a6b00d47f342e7ccc
bryce <112667970+BryceV1@users.noreply.github.com>
2024-05-10 11:09:04 -0600

    Provide solution for removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  const myArray = [];
+  /*
+  Create new array with ...args. For each element
+  check to see if it is not included in the args.
+  Then push remain elemnts onto array. 
+  */
+  array.forEach((element) => {
+    if (!args.includes(element)){
+        myArray.push(element)
+    }
+  }) 
+  return myArray;
 };
 
 // Do not edit below this line
