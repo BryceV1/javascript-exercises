ae602ce10fc3d096bcdadcbb69d4da81ce9bc066
bryce <112667970+BryceV1@users.noreply.github.com>
2024-05-10 12:25:00 -0600

    Provide Solution for sumAll
    The first section of lines will provide the checks
    for test cases where numbers are negative/not numbers
    etc. Once those are done, I used a simple for loop
    to iterate between the two provided numbers and simply
    add them to a finalsum variable.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function ( num1, num2) {
+  // Error if not num , as well as if num1/2 are negative
+  // swap num1 with num2 if the num1 is larger 
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (num1 > num2) {
+    const temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+  // iterate through num1 using num2 as the the last value
+  //add each number to our finalSum variable
+  let finalSum = 0;
+  for (let i = num1; i <= num2; i++) {
+    finalSum += i;
+  }
+  return finalSum;
 };
 
-// Do not edit below this line
 module.exports = sumAll;
