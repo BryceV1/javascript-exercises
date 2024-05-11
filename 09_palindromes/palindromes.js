dfdc1eede39c63cc4aa9c56a71b9ae9acf7f2f42
bryce <112667970+BryceV1@users.noreply.github.com>
2024-05-11 11:45:22 -0600

    Provide solution for palindromes
    After diving through different sites, I found
    that i should first take any word/s i am given
    and "clean" them. so used a regex to search through
    the word and see if it was only numbers/letters
    then the word would be put into lowercase.
    
    My solution is a bit different from TOP as their
    solution sets a variable to only allow certain
    chars.
    
    Once the cleanword is done, just needs to be
    split into separate chars, reveresed, and joined back.
    Then compare to the two words.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // Use only numbers/characters and convert to lowercase. Uses global flag /g
+    const cleanedWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+    // Reverse the cleaned word
+    const reversedWord = cleanedWord.split('').reverse().join('');
+    
+    // Check if the reversed word matches the original word
+    return cleanedWord === reversedWord;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
