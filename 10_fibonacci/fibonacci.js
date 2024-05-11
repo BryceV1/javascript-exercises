6386aeffa825be2080bfd4c2dfaa355647eb0e19
bryce <112667970+BryceV1@users.noreply.github.com>
2024-05-11 12:29:20 -0600

    provide solution to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+  // convert a string into a int, else its arleady an int
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
+// initial checks to make sure its no negative or 0.
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
 
+// iterates through our fib array
+//uses the count to output the number it ends at
+ const fib = [0, 1];
+ for (let i = 2; i <= count; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+ }
+ return fib[count];
 };
 
 // Do not edit below this line
